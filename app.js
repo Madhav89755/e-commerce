e171089f70e48abe489dd3d3d098d49f86f71533
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2024-07-21 14:59:13 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const sequelize = require("./database");
 const morgan = require('morgan');
 const app = express();
 
-const port = 3000;
+require("dotenv").config();
+
+const port = process.env.PORT||3000;
 
 // app.use(morgan('dev'))
 app.use("/", routes.router);
