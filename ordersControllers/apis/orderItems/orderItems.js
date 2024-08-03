39f4d80c7f33e5328a9a7804ac5077bd287edf3a
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2024-08-03 12:13:33 +0000

    added exception handling in orderItems.js

--- ordersControllers/apis/orderItems/orderItems.js
+++ ordersControllers/apis/orderItems/orderItems.js
@@ -16,25 +16,29 @@ async function addItemsToOrder(request, response) {
   // Add a new item to an unpaid order that is in created state
   const context = {};
   let status_code = status.CREATED;
-  const id = request.body.product_id;
-
-  const product = await ProductModel.findByPk(id);
-  if (product === null) {
-    status_code = status.NOT_FOUND;
-    context.message = messages.PRODUCT_NOT_FOUND;
-  } else {
-    const order = await fetch_unpaid_order(request.user.user_id);
-    const order_item = (
-      await OrderItem.findOrCreate({
-        where: {
-          product_id: id,
-          order_id: order.order_id,
-          user_id: request.user.user_id,
-        },
-      })
-    )[0];
-    context.order = await calculatePriceForOrder(order.order_id);
-    context.order_item = order_item;
+  try {
+    const id = request.body.product_id;
+    const product = await ProductModel.findByPk(id);
+    if (product === null) {
+      status_code = status.NOT_FOUND;
+      context.message = messages.PRODUCT_NOT_FOUND;
+    } else {
+      const order = await fetch_unpaid_order(request.user.user_id);
+      const order_item = (
+        await OrderItem.findOrCreate({
+          where: {
+            product_id: id,
+            order_id: order.order_id,
+            user_id: request.user.user_id,
+          },
+        })
+      )[0];
+      context.order = await calculatePriceForOrder(order.order_id);
+      context.order_item = order_item;
+    }
+  } catch (e) {
+    context.message = e.message;
+    status_code = status.BAD_REQUEST
   }
   response.status(status_code).json(context);
 }
@@ -43,51 +47,55 @@ async function updateItemQtyInOrder(request, response) {
   // Update the quantity of the item in an unpaid order
   const context = {};
   let status_code = status.CREATED;
-  const id = request.body.product_id;
-  const new_quantity = request.body.new_quantity;
-
-  const product = await ProductModel.findByPk(id);
-
-  if (product === null) {
-    status_code = status.NOT_FOUND;
-    context.message = messages.PRODUCT_NOT_FOUND;
-  } else {
-    const item_id = request.params.item_id;
-    const order_item = await OrderItem.findOne({where:{item_id, user_id:request.user.user_id}});
-    const order = await Order.findOne({
-      where: {
-        order_id: order_item.order_id,
-        status: "created",
-      },
-    });
-    if (order == null) {
-      context.message = messages.ORDER_PAID_OR_SHIPPED;
+  try {
+    const id = request.body.product_id;
+    const new_quantity = request.body.new_quantity;
+    const product = await ProductModel.findByPk(id);
+    if (product === null) {
+      status_code = status.NOT_FOUND;
+      context.message = messages.PRODUCT_NOT_FOUND;
     } else {
-      order_item.update({ quantity: new_quantity });
-
-      context.order = await calculatePriceForOrder(order.order_id);
-      context.order_item = order_item;
+      const item_id = request.params.item_id;
+      const order_item = await OrderItem.findOne({ where: { item_id, user_id: request.user.user_id } });
+      const order = await Order.findOne({
+        where: {
+          order_id: order_item.order_id,
+          status: "created",
+        },
+      });
+      if (order == null) {
+        context.message = messages.ORDER_PAID_OR_SHIPPED;
+      } else {
+        order_item.update({ quantity: new_quantity });
+        context.order = await calculatePriceForOrder(order.order_id);
+        context.order_item = order_item;
+      }
     }
+  } catch (e) {
+    context.message = e.message;
+    status_code = status.BAD_REQUEST
   }
   response.status(status_code).json(context);
 }
 
 async function removeItemFromOrder(request, response) {
-  let status_code = status.OK;
   const context = {};
-
-  const item_id = request.params.item_id;
-  const order_item = await OrderItem.findOne({item_id, user_id:request.user.user_id,});
-  if (order_item === null) {
-    context.message = messages.ORDER_ITEM_NOT_FOUND;
-    status_code = status.NOT_FOUND;
-  } else {
-    const order_id = order_item.order_id;
-
-    await order_item.destroy();
-    context.order = await calculatePriceForOrder(order_id);
-
-    context.message = messages.ORDER_ITEM_DELETED;
+  let status_code = status.OK;
+  try {
+    const item_id = request.params.item_id;
+    const order_item = await OrderItem.findOne({ item_id, user_id: request.user.user_id, });
+    if (order_item === null) {
+      context.message = messages.ORDER_ITEM_NOT_FOUND;
+      status_code = status.NOT_FOUND;
+    } else {
+      const order_id = order_item.order_id;
+      await order_item.destroy();
+      context.order = await calculatePriceForOrder(order_id);
+      context.message = messages.ORDER_ITEM_DELETED;
+    }
+  } catch (e) {
+    context.message = e.message;
+    status_code = status.BAD_REQUEST
   }
   response.status(status_code).json(context);
 }
