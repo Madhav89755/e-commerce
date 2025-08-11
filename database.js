7ed2439d37c5f454b643ea0e946df95409ad73b2
Madhav Sharma <80576996+Madhav89755@users.noreply.github.com>
2025-08-11 11:58:11 +0000

    Updated

--- database.js
+++ database.js
@@ -10,7 +10,7 @@ const sequelize = new Sequelize(
     host:process.env.DB_HOST,
     port:process.env.DB_PORT,
     logging:false,
-    dialect:process.env.DB_DIALECT,
+    dialect:"postgres",
     dialectOptions: {
       ssl: {
         require: true,
